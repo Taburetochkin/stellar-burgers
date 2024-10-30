0d56e88b9b01afe3fbed15c72c86648e2838f0de
Жуков Алексей <aazhukov_6@edu.hse.ru>
2024-10-31 01:58:45 +0300

    made first routes and configured .env

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,82 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders,
+  NotFound404
+} from '@pages';
+
+import {
+  AppHeader,
+  Modal,
+  OrderInfo,
+  IngredientDetails
+} from '@components';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { UseDispatch } from 'react-redux';
+import { AppDispatch } from 'src/services/store';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+const App = () => {
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <Routes>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route
+          path='/feed/:number'
+          element={
+            <Modal
+              title='lol1'
+              onClose={() => {
+              }}
+            >
+              <OrderInfo />
+            </Modal>
+          }
+        />
+        <Route
+          path='/ingredients/:id'
+          element={
+            <Modal
+              title='lol2'
+              onClose={() => {
+              }}
+            >
+              <IngredientDetails />
+            </Modal>
+          }
+        />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/profile/orders' element={<ProfileOrders />} />
+        <Route
+          path='/profile/orders/:number'
+          element={
+            <Modal
+              title='lol3'
+              onClose={() => {
+              }}
+            >
+              <OrderInfo />
+            </Modal>
+          }
+        />
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+    </div>
+  );  
+};
 
 export default App;
